103e9ae04f8d6ad94a73bdbed5ed5b1170d66d85
stefan <stefan.egli@gmail.com>
2025-06-07 20:10:06 +0200

    solved 06-02_vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -12,10 +12,23 @@
 // `fibonacci(2)` to return `1`, and so on.
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
-    //
+
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+
+    let mut fib_sequence = vec![0, 1];
+
+    for i in 2..=n {
+        let next_fib = fib_sequence[(i - 1) as usize] + fib_sequence[(i - 2) as usize];
+        fib_sequence.push(next_fib);
+    }
+    fib_sequence[n as usize]
 }
 
 #[cfg(test)]
